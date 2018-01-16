d418d8c3079118330d23bba51606f05f41c37186
Cédric L. Charlier <cedric.charlier@yucom.be>
2018-01-16 21:55:38 +0100

    Remove tests from CI

--- setup-neo4j.ps1
+++ setup-neo4j.ps1
@@ -1,4 +1,4 @@
-Import-Module "c:\projects\neo4j-community-2.3.2\bin\Neo4j-Management.psd1"
+Import-Module "c:\projects\neo4j\neo4j-community-3.3.0\bin\Neo4j-Management.psd1"
 
 function update-password
 {
@@ -13,9 +13,17 @@ function update-password
 
   Invoke-RestMethod -Uri $uri -Method Post -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -ContentType "application/json; charset=UTF-8" -Body $json
 }
+
+Write-Host 'Installing Neo4jServer ...'
  
-Install-Neo4jServer -Neo4jServer "c:\projects\neo4j-community-2.3.2" -PassThru | Start-Neo4jServer
+Install-Neo4jServer -Neo4jServer "c:\projects\neo4j\neo4j-community-3.3.0\" -PassThru | Start-Neo4jServer
+
+Write-Host 'Neo4jServer installed.'
 
 Start-Sleep -s 10
 
+Write-Host 'Updating password ...'
+
 update-password
+
+Write-Host 'Password updated.'
